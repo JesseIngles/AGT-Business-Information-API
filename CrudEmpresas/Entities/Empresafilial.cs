1916fed1bee9b6fd8f99641acf1dbccd9827f0c3
Jessé Inglês <JesseIngles@hotmail.com>
2024-02-17 23:39:45 +0100

    Fiz um database first, usando o cmd dotnet ef dbcontext scaffold ..., com isso temos as entidade )

---
+++ CrudEmpresas/Entities/Empresafilial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudEmpresas.Entities;
+
+public partial class Empresafilial
+{
+    public int Id { get; set; }
+
+    public string Logotipo { get; set; } = null!;
+
+    public string Firma { get; set; } = null!;
+
+    public string Nif { get; set; } = null!;
+
+    public int? Atividadeeconomicaid { get; set; }
+
+    public int? Empresaid { get; set; }
+
+    public virtual Empresa? Atividadeeconomica { get; set; }
+
+    public virtual Empresa? Empresa { get; set; }
+}
