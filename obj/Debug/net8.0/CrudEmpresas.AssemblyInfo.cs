84d38996141f023544f8e5e6bd3c7bf17da5a845
Jessé Inglês <JesseIngles@hotmail.com>
2024-03-08 23:01:42 +0100

    Mescaldno

--- obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d9f23fdcba38014e442518c34e07d40b43b074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+294c2ce5be96ae24489c5219a8813692efa56a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
