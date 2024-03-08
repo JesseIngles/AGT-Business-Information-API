cfba38b1273da1ecc63e0162b21f5b35a1dc5d04
Jessé Inglês <JesseIngles@hotmail.com>
2024-03-08 23:13:28 +0100

    Updates

---
+++ Entities/FuncionarioTelefone.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CrudEmpresas.Entities
+{
+
+    [Table("funcionariotelefone")]
+    public class funcionariotelefone
+    {
+
+
+        [Column("id")]
+        public int id { get; set; }
+        [Column("ddd")]
+        public string ddd { get; set; }
+        [Column("telefone")]
+        public int telefone { get; set; }
+
+
+
+
+
+
+    }
+}
