f90adaa108bb1d3779cdf8a579aeaadd9ac6505f
Jessé Inglês <JesseIngles@hotmail.com>
2024-03-10 00:58:55 +0100

    Commiting localchanges

--- obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68a602ba5fee2accc5bf2f3dad06fe2380436ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c3f3e5b57068dff4bb86e4d7c7faaebec397f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
