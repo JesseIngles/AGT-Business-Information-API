84a3be8a5fe75ffd17a9ef82573c110a5a0855d0
EllyNeto <eliandrasibo12@gmail.com>
2024-03-09 22:37:33 +0100

    UPDATE

--- obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudEmpresas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2673bcec2f68b6d580beaf0588421d12a541c0ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1782c9593a0c4ba9de9c1ce32524429ddd6f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudEmpresas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
